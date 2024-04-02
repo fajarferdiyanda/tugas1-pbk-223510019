e80ddddff2709cf63d9ff204071e3eb29c2a8fe3
Fajar <fajarferdianda@gmail.com>
2024-04-02 12:05:50 +0700

    commit1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Nama : Fajar Ferdiyanda <br />
+      Tempat/Tanggal Lahir : Pekanbaru/29 Agustus 2003 <br />
+      Jurusan : Teknik Informatika <br />
+      Pendidikan : Universitas Islam Riau <br />
+      Tahun Masuk : 2022 <br />
+
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener"></a>
+      <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+  color: black;
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  color: white;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Riwayat Pendidikan</template>
+
+    <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+
+    *SDN 015 Tandun, Rokan Hulu(2009-2012) <br />
+    *SDN 009 Tandun, Rokan Hulu(2012-2015) <br />
+    *SMPN 2 Tandun, Rokan Hulu(2015-2018) <br />
+    *SMAN 2 Tandun, Rokan Hulu(2018-2021)
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>keterampilan</template>
+    *Keterampilan Komunikasi <br />
+    *Keterampilan Analisis data <br />
+    *Keterampilan Manajemen Waktu
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Pengalaman kerja</template>
+    Asisten Peneliti <br />
+    ABC Institute for Research, Bandung, Indonesia <br />
+    Juni 2018 - Desember 2019 <br />
+
+    *Mendukung penelitian di bidang biologi molekuler dengan mengumpulkan dan menganalisis data
+    eksperimental. <br />
+    *Bertanggung jawab untuk memelihara stok dan kultur sel, serta melakukan percobaan laboratorium
+    sesuai dengan protokol penelitian. <br />
+    *Berkontribusi dalam penulisan laporan penelitian dan makalah ilmiah untuk publikasi jurnal.
+    <br />
+    *Mengkoordinasikan kolaborasi dengan universitas dan lembaga penelitian lainnya untuk pertukaran
+    data dan sumber daya.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Media Sosial</template>
+    Instagram : @fajarfrd
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Kontak</template>
+    fajarferdianda@gmail.com/082211901261
+  </WelcomeItem>
+</template>
